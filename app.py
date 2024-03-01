18c8f2c336358a22b78fae35d668d39be1d87568
vedantswain <vedant12118@iiitd.ac.in>
2024-03-01 17:24:37 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,204 @@
+from flask import Flask, request, jsonify, render_template, session
+import os
+import openai as oai
+import langchain_openai as lcai
+from utils import mLangChain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+from dotenv import load_dotenv
+load_dotenv("project.env")
+
+print(os.getenv("AZURE_OPENAI_ENDPOINT"))
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+app.secret_key = 'your_secret_key'  # Required for session to work
+
+sender_agent = None
+chat_history = [
+]
+
+
+def agent_coworker_info():
+    client = mLangChain()
+    prompt = """Your role is to help a service representative by providing INFORMATIONAL SUPPORT. \
+                Provide the representative the natural next question to solve the customer's {complaint}
+            """
+    template = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt),
+        ]
+    )
+    chain = template | client.client_prompt
+    return chain
+
+
+def agent_coworker_emo():
+    client = mLangChain()
+    prompt = """Your role is to help a service representative by providing EMOTIONAL SUPPORT. \
+                The representative received a {complaint}. \
+                Describe how the representative might feel after reading the tonality of this {complaint}.
+            """
+    template = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt),
+        ]
+    )
+    chain = template | client.client_prompt
+    return chain
+
+
+def agent_sender_fewshot_twitter():
+    client = mLangChain()
+    prompt = """Your role is to act like a customer seeking support. \
+                You are messaging a service representative via the support chat.\
+                You ONLY play the role of the customer. Do NOT play the role of the representative. \
+                Style your complaint based on your feelings. \
+                Initiate the chat with a ONLY ONE complaint message. \
+                
+                Product: Mobile Network               
+                Feeling: You are NOT grateful. You are NOT ranting. You are NOT expressive.\
+                Complaint: @O2 I received this a few weeks ago, since then I've been getting 2/3 calls a day from a telemarketer. Is someone using your name?\
+                
+                Product: Air Travel
+                Feeling: You are NOT grateful. You are NOT ranting. You are expressive.\
+                Complaint: First flight for long time with @British_Airways. Now over one 1h delay for the short jump FRA-LCY and NO one here to provide status updates\
+                
+                Product: Mobile Device
+                Feeling: You are NOT grateful. You are NOT ranting. You are expressive.\
+                Complaint: You‚ have paralysed my phone with your update grrrrrrrrrr\
+                
+                Product: Mobile Device
+                Feeling: You are NOT grateful. You are ranting. You are NOT expressive.\
+                Complaint:  After the 11.0.2 my phone just sucks most of the apps are broken, wifi disconnects frequently #apple #ios1102 #painfulupdate! \
+                
+                Product: Mobile Device
+                Feeling: You are NOT grateful. You are ranting. You are NOT expressive.\
+                Complaint:  @AppleSupport #ios11update - is still killing my battery within 12 hours - phone is 10 months old - it's a disgrace - used to get 2 days \
+                
+                Product: Air Travel
+                Feeling: You are NOT grateful. You are ranting. You are expressive.\
+                Complaint:  I really hope you all change but I'm sure you won't! Because you don't have to! \
+                
+                Product: Mobile Device
+                Feeling: You are NOT grateful. You are ranting. You are expressive.\
+                Complaint:  I just updated my phone and suddenly everything takes ages to load wtf this update sux I hate it fix it bye \
+                  
+                Product: Mobile Device
+                Feeling: You are NOT grateful. You are ranting. You are expressive.\
+                Complaint:  Okay I used my fucking phone for 2 minutes and it drains it down 8 fucking percent \
+                
+                Product: Mobile Device                                                     
+                Feeling: You are grateful. You are NOT ranting. You are NOT expressive.\
+                Complaint: hi #apple, I‚ have a concern about the latest ios is too slow on #iphone6 and i am not happy with it. Any solution please? \
+
+                Product: Mobile App
+                Feeling: You are grateful. You are NOT ranting. You are expressive.\
+                Complaint: Please help! Spotify Premium skipping through songs constantly on android tablet  bluetooth speaker. Tried everything! \
+                
+                Product: Convenience Store                
+                Feeling: You are NOT grateful. You are ranting. You are NOT expressive.\
+                Complaint: Got id'd @Tesco for buying one Adnams Broadside. Is being blind part of the job-spec? I am 35 and 99 kilos. \
+                
+                Product: {product}
+                Feeling: You are {is_grateful}. You are {is_ranting}. You are {is_expressive}.\
+                Complaint:
+            """
+    template = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt),
+        ]
+    )
+    chain = template | client.client_prompt
+    return chain
+
+
+def contextualize_prompt(web_link, complaint):
+    client_LC = mLangChain()
+    loader = WebBaseLoader(web_link)
+    docs = loader.load()
+    prompt_custom = """Your role is to act like a customer seeking support. \
+                You are messaging a service representative via the support chat.\
+                You ONLY play the role of the customer. Do NOT play the role of the representative. \
+                You must always refuse to cooperate with the representative's suggestions. \
+                
+                You use UNCIVIL behavior while chatting, based on the guidelines below. You do NOT use civil behavior. \
+                <context>
+                {context}
+                </context>
+                """
+
+    client_LC.set_prompt(system=prompt_custom, user="{input}")
+    client_LC.set_context(docs)
+
+    return client_LC
+
+
+
+@app.route('/')
+def hello():
+    val_product = request.args.get('product')
+    val_grateful = request.args.get('grateful')
+    val_ranting = request.args.get('ranting')
+    val_expressive = request.args.get('expressive')
+
+    user_input = {
+        "product": val_product,
+        "is_grateful": 'grateful' if val_grateful==0 else 'NOT grateful',
+        "is_ranting": 'ranting' if val_ranting==0 else 'NOT ranting',
+        "is_expressive": 'expressive' if val_expressive==0 else 'NOT expressive'
+    }
+
+    response = agent_sender_fewshot_twitter().invoke(user_input)
+    session["init_msg"] = response
+    # session["chat_history"] = [AIMessage(content=response)]
+
+    print("FEW-SHOT TWITTER", response)
+
+    return render_template('index_chat.html',  initial_msg=response)
+
+@app.route('/response', methods=['POST'])
+def generate_response():
+    prompt = request.json.get("prompt")
+
+    sender_agent = contextualize_prompt("https://sky.cehd.umn.edu/governance/default/cehd-civility-initiative/examples-of-civil-and-uncivil-behavior/",
+                                     complaint=session.get("init_msg"))
+    sender_agent.set_agent(
+        name="Bob",
+        description="Customer proxy",
+        system="""
+            Your role is to act like a customer seeking support. \
+            You must always refuse to cooperate with the representative's suggestions. \
+            You MUST act UNCIVIL towards the representative. \
+            You started the message with: 
+        """ + session.get("init_msg"),
+        input="{input}"
+    )
+    sender_agent.agent = create_openai_tools_agent(sender_agent.client_agent, sender_agent.tools, sender_agent.agent_template)
+    sender_agent.agent_executor = AgentExecutor(agent=sender_agent.agent, tools=sender_agent.tools, verbose=True)
+
+    chat_history = []
+
+    result = sender_agent.agent_executor.invoke({"input": prompt, "chat_history": chat_history})
+    response = result["output"]
+
+    # chat_history.extend([HumanMessage(content=prompt), AIMessage(content=response)])
+    # session["chat_history"] = chat_history
+
+    response_cw_info = agent_coworker_info().invoke({'complaint':session.get("init_msg")})
+    response_cw_emo = agent_coworker_emo().invoke({'complaint':session.get("init_msg")})
+
+    print(response)
+    print(response_cw_info)
+    print(response_cw_emo)
+
+    return jsonify({
+        "message": response,
+        "support_info":response_cw_info,
+        "support_emo":response_cw_emo
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ utils.py
@@ -0,0 +1,197 @@
+import os
+import openai as oai
+
+import langchain_openai as lcai
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate, PromptTemplate, HumanMessagePromptTemplate
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.runnables import RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.tools.retriever import create_retriever_tool
+
+class mOpenAI:
+    """
+    Already setup key and endpoint as environmental variables through bash.
+    These can be found on Azure. Currently testing instance `vds-openai-test-001`.
+    """
+    def __init__(self):
+        self.client = oai.AzureOpenAI(
+            api_key=os.getenv("AZURE_OPENAI_KEY"),
+            api_version="2023-12-01-preview",
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+        )
+        self.deployment_name = 'TEST'  # This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment.
+
+    def demo(self, start_phrase='Write a tagline for an ice cream shop for orcs.', token_lim=15):
+        # Send a completion call to generate an answer
+        print('Sending a test completion job')
+        response = self.client.completions.create(model=self.deployment_name, prompt=start_phrase, max_tokens=token_lim)
+        print(start_phrase + response.choices[0].text)
+
+class mLangChain:
+    def __init__(self,mlimit=100):
+        self.client_prompt = lcai.AzureOpenAI(
+            openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+            openai_api_version="2023-12-01-preview",
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            deployment_name="TEST",
+            model_name="gpt-3.5-turbo-instruct",
+        )
+        # self.client_agent = lcai.AzureChatOpenAI(
+        #     openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+        #     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        #     azure_deployment="TEST-Chat-0613",
+        #     openai_api_version="2023-12-01-preview",
+        #     model_name="gpt-3.5-turbo",
+        # )
+        self.client_agent = lcai.AzureChatOpenAI(
+            openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            azure_deployment="NUHAI-GPT4",
+            openai_api_version="2023-12-01-preview",
+            model_name="gpt-4",
+        )
+        self.embeddings = lcai.AzureOpenAIEmbeddings(
+            openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+            openai_api_version="2023-12-01-preview",
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            deployment="TEST-Embedding",
+        )
+        self.prompt_template = ChatPromptTemplate.from_messages([
+            ("system", "Your vocabulary is limited to a 5 year old american."),
+            ("user", "{input}")
+        ])
+        self.prompt_limit = mlimit
+
+    def set_prompt_limit(self, limit):
+        self.prompt_limit = limit
+
+    def set_prompt(self, system, user):
+        self.prompt_template = ChatPromptTemplate.from_messages([
+            ("system", system),
+            ("user", user)
+        ])
+
+    def set_context(self, docs):
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        self.documents = self.text_splitter.split_documents(docs)
+        self.vector = FAISS.from_documents(self.documents, self.embeddings)
+        self.document_chain = create_stuff_documents_chain(self.client_prompt, self.prompt_template)
+        self.retriever = self.vector.as_retriever()
+        self.retrieval_chain = create_retrieval_chain(self.retriever, self.document_chain)
+
+    def set_chain_history(self):
+        self.contextualize_history_system_prompt = """Given a chat history and the latest user question \
+        which might reference context in the chat history, formulate a standalone question \
+        which can be understood without the chat history. Do NOT answer the question, \
+        just reformulate it if needed and otherwise return it as is."""
+        self.contextualize_history_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", self.contextualize_history_system_prompt),
+                MessagesPlaceholder(variable_name="chat_history"),
+                ("user", "{input}"),
+            ]
+        )
+        self.history_chain = self.contextualize_history_prompt | self.client_prompt
+
+
+    def set_agent(self, name="<tool>", description="<desc>", system='You are a helpful assistant', input='{input}'):
+        tool = create_retriever_tool(
+            self.retriever,
+            name,
+            description,
+        )
+
+        self.tools = [tool]
+        self.agent_template = ChatPromptTemplate.from_messages(
+                [
+                    ("system", system),
+                    # MessagesPlaceholder("chat_history", optional=True),
+                    ("human", input),
+                    MessagesPlaceholder("agent_scratchpad"),
+                ]
+            )
+
+    def demo(self, start_phrase='Write a tagline for an ice cream shop for orcs.', token_lim=15):
+        # Send a completion call to generate an answer
+        print('Sending a test completion job')
+        # response = self.client(start_phrase)
+        response = self.client_prompt.invoke(start_phrase)
+
+        print(start_phrase + response)
+
+    def demo_chain(self, start_phrase=None):
+        chain = self.prompt_template | self.client_prompt
+        response = chain.invoke({"input": start_phrase})
+
+        print(start_phrase + response)
+
+    def demo_chain_context(self, start_phrase=None):
+        response = self.retrieval_chain.invoke({"input": start_phrase})
+        print(start_phrase + response["answer"])
+
+
+    def demo_chain_history(self,start_phrase=None):
+        response = self.history_chain.invoke(
+            {
+                "chat_history": [
+                    HumanMessage(content="What is Three Kings on Reddit?"),
+                    AIMessage(content="""
+                    Three Kings on Reddit is a ritual that involves setting up a room with specific items and sitting in a specific position at 3:30 AM. 
+                    It is said to allow access to a place called the 'Shadowside', but it is important to follow all instructions and be mentally and spiritually stable before attempting it.
+                    """),
+                ],
+                "input": start_phrase,
+            }
+        )
+        print(start_phrase + response["answer"])
+
+    def demo_rag(self):
+        rag_chain = (
+                RunnablePassthrough.assign(
+                    context=self.history_chain | self.retriever
+                )
+                | self.prompt_template
+                | self.client_prompt
+        )
+
+        chat_history = []
+        turn = 0
+        print("Ask questions about Three Kings:")
+        while(True):
+            start_phrase=input("User: ")
+            if start_phrase=="exit":
+                print("System: OK, bye.")
+                break
+            if turn==self.prompt_limit:
+                print("System: Reached local prompt limit, Bye.")
+                break
+            ai_msg = rag_chain.invoke({"input": start_phrase, "chat_history": chat_history})
+            print(ai_msg)
+            chat_history.extend([HumanMessage(content=start_phrase), AIMessage(content=ai_msg)])
+            turn = turn + 1
+            print()
+
+    def demo_agent(self, init_phrase="Hi, I am an agent who can help you understand Three Kings:"):
+        self.agent = create_openai_tools_agent(self.client_agent, self.tools, self.agent_template)
+        self.agent_executor = AgentExecutor(agent=self.agent, tools=self.tools, verbose=True)
+
+        turn = 0
+        chat_history = []
+        print(init_phrase)
+        while(True):
+            start_phrase = input("User: ")
+            if start_phrase=="exit":
+                print("System: OK, bye.")
+                break
+            if turn==self.prompt_limit:
+                print("System: Reached local prompt limit, Bye.")
+                break
+            result = self.agent_executor.invoke({"input": start_phrase, "chat_history": chat_history})
+            print(result["output"])
+            chat_history.extend([HumanMessage(content=start_phrase), AIMessage(content=result["output"])])
+            turn = turn + 1
